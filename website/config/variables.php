8541af00bf883f2df72316ce89fe65c1f6b52001
Sigui Kessé Emmanuel <contact@sigui.ci>
2024-04-06 02:54:30 +0100

    :tada: Initial scaffolding

---
+++ website/config/variables.php
@@ -0,0 +1,29 @@
+<?php
+// Variables
+return [
+  "creatorName" => "Sigui",
+  "creatorUrl" => "https://siguici.deno.dev",
+  "templateName" => "Sikessem",
+  "templateSuffix" => "Your digital partner",
+  "templateVersion" => "1.0.0",
+  "templateFree" => false,
+  "templateDescription" => "Start your development with a Dashboard for Bootstrap 5",
+  "templateKeyword" => "dashboard, bootstrap 5 dashboard, bootstrap 5 design, bootstrap 5",
+  "licenseUrl" => "https://themeforest.net/licenses/standard",
+  "livePreview" => "https://demos.pixinvent.com/vuexy-html-admin-template/landing/",
+  "productPage" => "https://1.envato.market/vuexy_admin",
+  "support" => "https://pixinvent.ticksy.com/",
+  "moreThemes" => "https://1.envato.market/pixinvent_portfolio",
+  "documentation" => "https://demos.pixinvent.com/vuexy-html-admin-template/documentation",
+  "generator" => "",
+  "changelog" => "https://demos.pixinvent.com/vuexy/changelog.html",
+  "repository" => "https://github.com/pixinvent/vuexy-html-admin-template",
+  "gitRepo" => "pixinvent",
+  "gitRepoAccess" => "vuexy-html-admin-template",
+  "githubFreeUrl" => "https://tools.pixinvent.com/github/github-access",
+  "facebookUrl" => "https://www.facebook.com/siguici/",
+  "twitterUrl" => "https://twitter.com/siguici",
+  "githubUrl" => "https://github.com/siguici",
+  "dribbbleUrl" => "https://dribbble.com/siguici",
+  "instagramUrl" => "https://www.instagram.com/siguici/"
+];
