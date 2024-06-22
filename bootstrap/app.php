d3bc2855d4c472e007fe9019ebd1ecdea99e919e
Sigui Kessé Emmanuel <siguici@proton.me>
2024-06-22 05:54:41 +0100

    :zap: Use the sikessem/framework instead of laravel/framework

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Sikessem\Application;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
