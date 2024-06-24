e8ac04c1532e25c0b72eda7751df62b9a1b989d0
Sigui Kessé Emmanuel <siguici@proton.me>
2024-06-24 05:49:57 +0100

    :truck: Rename the modal namespace to Vuexy\Modal

--- app/Http/Controllers/Vuexy/Modal/ModalExample.php
+++ app/Http/Controllers/Vuexy/Modal/ModalExample.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\modal;
+namespace App\Http\Controllers\Vuexy\Modal;
 
 use App\Http\Controllers\Controller;
 
@@ -8,6 +8,6 @@ class ModalExample extends Controller
 {
     public function index()
     {
-        return view('content.modal.modal-examples');
+        return view('content.vuexy.modal.examples');
     }
 }
