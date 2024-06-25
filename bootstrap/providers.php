7cccba10c185c0bdc604fca2b65e3ffd3594eb3b
Sigui Kessé Emmanuel <siguici@proton.me>
2024-06-25 01:55:00 +0100

    :heavy_plus_sign: Add all application providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,13 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\AuthServiceProvider::class,
+    App\Providers\BroadcastServiceProvider::class,
+    App\Providers\EventServiceProvider::class,
     App\Providers\FolioServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
+    App\Providers\MenuServiceProvider::class,
+    App\Providers\RouteServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
 ];
