0fe5a93c262c3251aa3848b915abc4fc3d0499e3
Sigui Kessé Emmanuel <siguici@proton.me>
2024-06-29 20:27:11 +0100

    :hammer: Improve the use of TypeScript

--- resources/design/types/index.d.ts
+++ resources/design/types/index.d.ts
@@ -1,9 +1,13 @@
 import type { Alpine } from 'alpinejs';
 import type Axios from 'axios';
+import type Echo from 'laravel-echo';
+import type Pusher from 'pusher-js';
 
 declare global {
   interface Window {
     Alpine: Alpine;
     axios: Axios;
+    Pusher: Pusher;
+    Echo: Echo;
   }
 }
