caa4480bf414ed94ee7257bd6269741821dba1ca
Sigui Kessé Emmanuel <siguici@proton.me>
2024-06-29 19:27:52 +0100

    :building_construction: Improve the bootstrapper structure

---
+++ app/Bootstrappers/MiddlewareBootstrapper.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Bootstrappers;
+
+use Illuminate\Foundation\Configuration\Middleware;
+
+class MiddlewareBootstrapper
+{
+    public function __invoke(Middleware $middleware): void
+    {
+        //
+    }
+}
