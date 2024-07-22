5d4b8cd142f84eb9da425b63c01c7b8a47429987
Sigui Kessé Emmanuel <siguici@proton.me>
2024-07-22 11:02:27 +0100

    :zap: Create the preloader only if it does not exist

--- resources/design/preload.ts
+++ resources/design/preload.ts
@@ -1,11 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const preloader = document.createElement('div');
-  const brand = document.createElement('img');
+  let preloader = document.getElementById('preloader');
+  if (!preloader) {
+    preloader = document.createElement('div');
+    preloader.id = 'preloader';
+    document.body.appendChild(preloader);
+  }
 
-  preloader.id = 'preloader';
-  brand.classList.add('brand');
-  preloader.appendChild(brand);
-  document.body.appendChild(preloader);
+  let brand = document.querySelector('#preloader .brand');
+  if (!brand) {
+    brand = document.createElement('img');
+    brand.classList.add('brand');
+    preloader.appendChild(brand);
+  }
 
   window.addEventListener('load', function() {
     preloader.classList.add('loaded');
