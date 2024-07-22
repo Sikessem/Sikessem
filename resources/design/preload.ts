ccee811895c451a87fd144b36c0d8c5fc50e10e0
Sigui Kessé Emmanuel <siguici@proton.me>
2024-07-22 10:04:16 +0100

    :lipstick: Improve the preloader style

--- resources/design/preload.ts
+++ resources/design/preload.ts
@@ -1,5 +1,9 @@
 const preloader = document.createElement('div');
+const brand = document.createElement('img');
+
 preloader.id = 'preloader';
+brand.classList.add('brand');
+preloader.appendChild(brand);
 document.body.appendChild(preloader);
 
 window.addEventListener('load', function() {
