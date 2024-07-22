8dca9268714544b421cbea1bc3576910308e9d07
Sigui Kessé Emmanuel <siguici@proton.me>
2024-07-22 10:36:22 +0100

    :zap: Create preloader when the DOM is loaded

--- resources/design/preload.ts
+++ resources/design/preload.ts
@@ -1,15 +1,17 @@
-const preloader = document.createElement('div');
-const brand = document.createElement('img');
+document.addEventListener('DOMContentLoaded', function() {
+  const preloader = document.createElement('div');
+  const brand = document.createElement('img');
 
-preloader.id = 'preloader';
-brand.classList.add('brand');
-preloader.appendChild(brand);
-document.body.appendChild(preloader);
+  preloader.id = 'preloader';
+  brand.classList.add('brand');
+  preloader.appendChild(brand);
+  document.body.appendChild(preloader);
 
-window.addEventListener('load', function() {
-  preloader.classList.add('loaded');
+  window.addEventListener('load', function() {
+    preloader.classList.add('loaded');
 
-  preloader.addEventListener('transitionend', () => {
-    preloader.remove();
+    preloader.addEventListener('transitionend', () => {
+      preloader.remove();
+    });
   });
 });
