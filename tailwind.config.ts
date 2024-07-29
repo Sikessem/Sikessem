fba25fa6bbf84370236a26c7f6bb7fd6dbc1a9f3
Sigui Kessé Emmanuel <siguici@proton.me>
2024-07-29 07:38:48 +0100

    :zap: Migrate to Tailwind v4.0

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,39 +1,16 @@
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 import styliz from 'styliz';
-import defaultTheme from 'tailwindcss/defaultTheme';
-
-class Fonts {
-  protected variants: Record<string, string[]> = {};
-
-  all(): Record<string, string[]> {
-    return this.variants;
-  }
-
-  add(name: string, variable = false): this {
-    this.variants[name] = Fonts.variants_of(name, variable);
-    return this;
-  }
-
-  static variants_of(name: string, variable = false): string[] {
-    const variant = name
-      .split('-')
-      .map((name) => name[0].toUpperCase() + name.slice(1))
-      .join(' ');
-    return [
-      variable ? `${variant} Variable` : variant,
-      ...defaultTheme.fontFamily.sans,
-    ];
-  }
-}
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  theme: {
-    extend: {
-      fontFamily: new Fonts().add('figtree', true).add('rajdhani').all(),
-    },
-  },
+  content: [
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './vendor/laravel/jetstream/**/*.blade.php',
+    './resources/views/**/*',
+    './storage/framework/views/*.php',
+    './app/View/**/*',
+  ],
 
   plugins: [forms, typography, styliz],
 };
