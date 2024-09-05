bba8bf0bcf4fc9fc600217e3f1d80b8ee9274aef
Sigui Kessé Emmanuel <siguici@proton.me>
2024-09-05 23:03:23 +0100

    :art: Use the global wrapper

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,35 +4,33 @@ use function Laravel\Folio\name;
 name('home');
 ?>
 <x-base-layout id="home" title="Welcome">
-    <div class="wrapper">
-        <header>
-            <nav class="container">
-                <x-application-brand height="40" />
-                <div class="auth-buttons">
-                    <a href="{{ route('login') }}" class="cta-button login-btn">Connexion</a>
-                    <a href="{{ route('register') }}" class="cta-button register-btn">Inscription</a>
-                </div>
-            </nav>
-        </header>
-
-        <main>
-            <section id="home" class="hero">
-                <div class="bg-grid"></div>
-                <div class="orb-container">
-                    <div class="orb main-orb"></div>
-                </div>
-                <div class="hero-content container">
-                    <h1>La Plateforme Numérique de Nouvelle Génération</h1>
-                    <p>Réalisez l'impossible avec {{ config('app.title') }} : <b>Donnez vie à vos rêves !</b></p>
-                    <a href="{{ route('auth') }}" class="cta-button">Démarrer l'aventure maintenant</a>
-                </div>
-            </section>
-        </main>
-
-        <footer>
-            <div class="container">
-                <p>&copy; <time datetime="{{ date('Y') }}">{{ date('Y') }}</time> {{ config('app.title') }}. Tous droits réservés.</p>
+    <header>
+        <nav class="container">
+            <x-application-brand height="40" />
+            <div class="auth-buttons">
+                <a href="{{ route('login') }}" class="cta-button login-btn">Connexion</a>
+                <a href="{{ route('register') }}" class="cta-button register-btn">Inscription</a>
             </div>
-        </footer>
-    </div>
+        </nav>
+    </header>
+
+    <main>
+        <section id="home" class="hero">
+            <div class="bg-grid"></div>
+            <div class="orb-container">
+                <div class="orb main-orb"></div>
+            </div>
+            <div class="hero-content container">
+                <h1>La Plateforme Numérique de Nouvelle Génération</h1>
+                <p>Réalisez l'impossible avec {{ config('app.title') }} : <b>Donnez vie à vos rêves !</b></p>
+                <a href="{{ route('auth') }}" class="cta-button">Démarrer l'aventure maintenant</a>
+            </div>
+        </section>
+    </main>
+
+    <footer>
+        <div class="container">
+            <p>&copy; <time datetime="{{ date('Y') }}">{{ date('Y') }}</time> {{ config('app.title') }}. Tous droits réservés.</p>
+        </div>
+    </footer>
 </x-base-layout>
