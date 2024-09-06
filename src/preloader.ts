ff79bef992952c47deda2293ed3b4703da6d5a44
Sigui Kessé Emmanuel <siguici@proton.me>
2024-09-06 13:40:00 +0100

    :dizzy: Fix navigation preloader

--- src/preloader.ts
+++ src/preloader.ts
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+const loaded = () => {
   let preloader = document.getElementById('preloader');
   if (!preloader) {
     preloader = document.createElement('div');
@@ -7,25 +7,26 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.insertBefore(preloader, document.body.firstChild);
   }
 
-  window.addEventListener('load', () => {
-    if (document.body.contains(preloader)) {
-      preloader.classList.add('loaded');
+  if (document.body.contains(preloader)) {
+    preloader.classList.add('loaded');
 
-      const timeoutId = setTimeout(() => {
-        if (window.requestAnimationFrame) {
-          window.requestAnimationFrame(() => remove(preloader));
-        } else {
-          remove(preloader);
-        }
-      }, 1000);
-
-      preloader.addEventListener('transitionend', () => {
-        clearTimeout(timeoutId);
+    const timeoutId = setTimeout(() => {
+      if (window.requestAnimationFrame) {
+        window.requestAnimationFrame(() => remove(preloader));
+      } else {
         remove(preloader);
-      });
-    }
-  });
-});
+      }
+    }, 1000);
+
+    preloader.addEventListener('transitionend', () => {
+      clearTimeout(timeoutId);
+      remove(preloader);
+    });
+  }
+};
+
+document.addEventListener('DOMContentLoaded', loaded);
+document.addEventListener('livewire:navigated', loaded);
 
 function remove(element: HTMLElement): void {
   if (!element) {
