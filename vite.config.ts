83667aafb5ca235963c0ecc9cf5a7e5d75baf6c3
Sigui Kessé Emmanuel <siguici@proton.me>
2024-09-11 06:07:35 +0100

    :rotating_light: Target ESNext

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  build: {
+    target: 'esnext',
+  },
   plugins: [
     laravel({
       ssr: 'src/ssr.js',
