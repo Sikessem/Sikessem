3f73c96a73536c4efa0b4c67ff8f29298a185a5f
Sigui Kessé Emmanuel <siguici@proton.me>
2024-10-08 01:09:17 +0100

    :bento: Update the application logo

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\LanguageController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Vite;
+
+Route::get('logo', fn() => '<img src="'.Vite::asset('src/images/logo.svg').'" alt="'.config('app.title').'"/>')->name('logo');
 
 Route::view('auth', 'auth.index')->name('auth');
 Route::post('auth', AuthController::class);
